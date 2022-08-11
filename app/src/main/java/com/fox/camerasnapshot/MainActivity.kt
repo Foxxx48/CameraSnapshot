042866806cd48a0bdff5cc139bb153ace603f65c
Foxxx48 <Foxxdim@mail.ru>
2022-08-11 16:44:19 +0300

    Activity Result API build ok

--- app/src/main/java/com/fox/camerasnapshot/MainActivity.kt
+++ app/src/main/java/com/fox/camerasnapshot/MainActivity.kt
@@ -16,50 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     private val PIC_ID = 123
 
+    val myStartForResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback {
+            val photo = it?.data?.extras?.get("data") as Bitmap?
+            println(photo)
+            binding.clickImage.setImageBitmap(photo)
+        })
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myStartForResult = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult(),
-            ActivityResultCallback {
-
-//                if (it.resultCode == PIC_ID) {
-
-                    val photo = it?.data?.extras?.get("data") as Bitmap?
-                    println(photo)
-
-
-                    binding.clickImage.setImageBitmap(photo)
-//                }
-            })
-
         binding.cameraButton.setOnClickListener {
-
-
-
-            var cameraIntent = Intent(
+            val cameraIntent = Intent(
                 MediaStore
                 .ACTION_IMAGE_CAPTURE)
 
-                myStartForResult.launch(cameraIntent)
-//            startActivityForResult(cameraIntent, PIC_ID)
+                myStartForResultLauncher.launch(cameraIntent)
+
         }
     }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        // Match the request 'pic id with requestCode
-//        if (requestCode == PIC_ID) {
-//
-//            // BitMap is data structure of image file
-//            // which stor the image in memory
-//            val photo = data?.extras?.get("data") as Bitmap?
-//
-//            // Set the image in imageview for display
-//            binding.clickImage.setImageBitmap(photo)
-//        }
-//    }
 }
